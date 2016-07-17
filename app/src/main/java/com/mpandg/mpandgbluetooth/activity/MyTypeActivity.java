8334796e73bdf8abd1d7182762963e1ff077a8d1
Ali Kabiri <ali@kabiri.org>
2016-07-18 01:08:56 +0430

    handled type item clicks

--- app/src/main/java/com/mpandg/mpandgbluetooth/activity/MyTypeActivity.java
+++ app/src/main/java/com/mpandg/mpandgbluetooth/activity/MyTypeActivity.java
@@ -15,22 +15,61 @@ public class MyTypeActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_type);
 
+        // register onClick listeners for card views.
+
         CardView general = (CardView) findViewById(R.id.card_general_user);
         general.setOnClickListener(this);
+
+        CardView bodyBuilder = (CardView) findViewById(R.id.card_body_builder);
+        bodyBuilder.setOnClickListener(this);
+
+        CardView runner = (CardView) findViewById(R.id.card_runner);
+        runner.setOnClickListener(this);
+
+        CardView biker = (CardView) findViewById(R.id.card_biker);
+        biker.setOnClickListener(this);
+
+        CardView aged = (CardView) findViewById(R.id.card_aged);
+        aged.setOnClickListener(this);
+
+        CardView addType = (CardView) findViewById(R.id.card_add_type);
+        addType.setOnClickListener(this);
     }
 
+    /**
+     * Handle cardView clicks to open corresponding activities.
+     * @param view
+     */
     @Override
     public void onClick(View view) {
 
         BodyType bodyType;
         switch (view.getId()) {
-
             case R.id.card_general_user:
-                bodyType = new BodyType();
-                bodyType.setColor(R.color.general_user);
-                bodyType.setName(getResources().getString(R.string.general_user));
-                bodyType.setPhoto(R.drawable.general_user);
+                // send the corresponding type for navigation purposes.
+                bodyType = new BodyType(getResources().getString(R.string.general_user), R.color.general_user, R.drawable.general_user);
                 TypeDetailActivity.navigate(this, view.findViewById(R.id.iv_general_user), bodyType);
+                break;
+            case R.id.card_body_builder:
+                bodyType = new BodyType(getResources().getString(R.string.body_builder), R.color.body_builder, R.drawable.bodybuilder);
+                TypeDetailActivity.navigate(this, view.findViewById(R.id.iv_body_builder), bodyType);
+                break;
+            case R.id.card_runner:
+                bodyType = new BodyType(getResources().getString(R.string.runner), R.color.runner, R.drawable.runner);
+                TypeDetailActivity.navigate(this, view.findViewById(R.id.iv_runner), bodyType);
+                break;
+            case R.id.card_biker:
+                bodyType = new BodyType(getResources().getString(R.string.biker), R.color.biker, R.drawable.biker);
+                TypeDetailActivity.navigate(this, view.findViewById(R.id.iv_biker), bodyType);
+                break;
+            case R.id.card_aged:
+                bodyType = new BodyType(getResources().getString(R.string.aged), R.color.aged, R.drawable.aged);
+                TypeDetailActivity.navigate(this, view.findViewById(R.id.iv_aged), bodyType);
+                break;
+            case R.id.card_add_type:
+                bodyType = new BodyType(getResources().getString(R.string.add_new_type), R.color.primary, R.drawable.ic_add_circle_outline_white_48dp);
+                TypeDetailActivity.navigate(this, view.findViewById(R.id.iv_add_new), bodyType);
+                break;
         }
     }
 }
--- app/src/main/java/com/mpandg/mpandgbluetooth/activity/TypeDetailActivity.java
+++ app/src/main/java/com/mpandg/mpandgbluetooth/activity/TypeDetailActivity.java
@@ -19,6 +19,7 @@ import android.transition.Slide;
 import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
