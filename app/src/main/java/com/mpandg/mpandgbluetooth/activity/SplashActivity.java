75486db19857e5f204fa6cdcc34eb505855ad51b
Ali Kabiri <ali@kabiri.org>
2016-07-19 00:27:15 +0430

    Added registration logic to SplashActivity

--- app/src/main/java/com/mpandg/mpandgbluetooth/activity/SplashActivity.java
+++ app/src/main/java/com/mpandg/mpandgbluetooth/activity/SplashActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.mpandg.mpandgbluetooth.Const;
 import com.mpandg.mpandgbluetooth.R;
 import com.mpandg.mpandgbluetooth.Utils;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private Button btRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +34,10 @@ public class SplashActivity extends AppCompatActivity {
 
                     // open main activity.
                     startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    // close this activity
+                    finish();
                 }
 
-                // close this activity
-                finish();
             }
         }, 1000);
     }
@@ -46,6 +50,21 @@ public class SplashActivity extends AppCompatActivity {
             // the user has registered successfully; invalidate first time use and open main activity.
             Utils.invalidateFirstTimeUse(this);
             startActivity(new Intent(this, MainActivity.class));
+            // close this activity
+            finish();
+        } else {
+
+            // user has not registered, keep this activity and show registration button.
+            btRegister = (Button) findViewById(R.id.bt_register);
+            btRegister.setVisibility(View.VISIBLE);
+            btRegister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    // open registration activity.
+                    startActivityForResult(new Intent(SplashActivity.this, RegistrationActivity.class), Const.REQUEST_CODE_REGISTRATION);
+                }
+            });
         }
     }
 }
