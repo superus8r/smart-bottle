1b3802308be4e8e5fc6b8fd34974b209b224d488
Ali Kabiri <ali@kabiri.org>
2016-07-18 23:08:05 +0430

    Added SplashActivity

---
+++ app/src/main/java/com/mpandg/mpandgbluetooth/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.mpandg.mpandgbluetooth.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mpandg.mpandgbluetooth.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
